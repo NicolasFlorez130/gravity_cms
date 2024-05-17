a21a113149e61407b11bb211820fa13c56febb14
Nicolas <nmflorezr@gmail.com>
2024-05-17 21:16:52 +0100

    Set up the base components and styles for the landing page

---
+++ src/components/ui/landing/button.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+   "rounded-full font-dm_sans focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+   {
+      variants: {
+         variant: {
+            primary: "border-3 border-primary p-6 text-primary-foreground",
+            secondary: "border p-3 border-secondary text-secondary",
+            highlight:
+               "border-3 border-primary p-6 text-black bg-primary-foreground",
+         },
+      },
+      defaultVariants: {
+         variant: "highlight",
+      },
+   },
+);
+
+export interface ButtonProps
+   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+      VariantProps<typeof buttonVariants> {
+   asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+   ({ className, variant, asChild = false, ...props }, ref) => {
+      const Comp = asChild ? Slot : "button";
+      return (
+         <Comp
+            className={cn(buttonVariants({ variant, className }))}
+            ref={ref}
+            {...props}
+         />
+      );
+   },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
