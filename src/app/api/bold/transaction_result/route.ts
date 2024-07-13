dca7eeb9e6e6594134a6d24c70793daf8a82c418
Nicolas <nicolas@Sheryls-MacBook-Pro-2.local>
2024-07-13 17:42:31 +0100

    Prepare the webhook for bold

--- src/app/api/bold/transaction_result/route.ts
+++ src/app/api/bold/transaction_result/route.ts
@@ -1,2 +1,59 @@
-export const dynamic = "force-dynamic"; // defaults to auto
-export async function POST(req: Request, res: Response) {}
+import { headers } from "next/headers";
+import { db } from "~/server/db";
+import { appointmentConfirmations } from "~/server/db/schemas/appointments";
+import { createHmac, timingSafeEqual } from "crypto";
+import { env } from "~/env";
+
+export const dynamic = "force-dynamic";
+export async function POST(req: Request) {
+   const headersList = headers();
+
+   const signature = headersList.get("x-bold-signature") ?? "";
+
+   const body = (await req.json()) as TransactionResult;
+
+   const strMessage = JSON.stringify(body);
+
+   const encoded = Buffer.from(strMessage).toString("base64");
+
+   const hashed = createHmac("sha256", env.BOLD_SECRET_KEY)
+      .update(encoded)
+      .digest("hex");
+
+   const isValidRequest = timingSafeEqual(
+      Buffer.from(hashed),
+      Buffer.from(signature),
+   );
+
+   console.log("isValidRequest:", isValidRequest);
+
+   //TODO: toggle this
+   // if (isValidRequest) {
+   if (true) {
+      if (body.type === "SALE_APPROVED") {
+         console.log("body request:", body);
+
+         try {
+            const insertResponse = await db
+               .insert(appointmentConfirmations)
+               .values({
+                  bookingId: body.data.payment_id,
+                  paymentReference: body.id,
+               })
+               .returning({ id: appointmentConfirmations.id });
+
+            console.log("insertResponse:", insertResponse);
+
+            Response.json(insertResponse);
+         } catch (error) {
+            return new Response(null, {
+               status: 500,
+            });
+         }
+      }
+   } else {
+      return new Response("Invalid signature", {
+         status: 400,
+      });
+   }
+}
---
+++ src/types/bold.d.ts
@@ -0,0 +1,50 @@
+interface TransactionResult {
+   id: string;
+   type: TransactionResultType;
+   subject: string;
+   source: string;
+   spec_version: string;
+   time: number;
+   data: Data;
+   datacontenttype: string;
+}
+
+type TransactionResultType =
+   | "SALE_APPROVED"
+   | "SALE_REJECTED"
+   | "VOID_APPROVED"
+   | "VOID_REJECTED";
+
+interface Data {
+   payment_id: string;
+   merchant_id: string;
+   created_at: Date;
+   amount: Amount;
+   card: Card;
+   user_id: string;
+   payment_method: string;
+   metadata: Metadata;
+}
+
+interface Amount {
+   total: number;
+   taxes: Tax[];
+   tip: number;
+}
+
+interface Tax {
+   base: number;
+   type: string;
+   value: number;
+}
+
+interface Card {
+   capture_mode: string;
+   franchise: string;
+   cardholder_name: string;
+   terminal_id: string;
+}
+
+interface Metadata {
+   reference: null;
+}
